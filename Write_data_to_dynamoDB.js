1ce81b18ee32b4ea5cb139ef8bf561ae5a54e580
madhuri1996 <madhuridoppalapudi4@gmail.com>
2017-12-28 09:06:01 +0530

    Writing data into DynamoDB

---
+++ Write_data_to_dynamoDB.js
@@ -0,0 +1,18 @@
+console.log('starting function');
+const AWS=require('aws-sdk');
+const docClient=new AWS.DynamoDB.DocumentClient({region: 'eu-west-1'});
+exports.handle=function(e,ctx,callback) {
+	var params = {
+	Item: {
+	date:Date.now(),
+	},TableName: 'csvdata'
+	};
+	docClient.put(params, function(err, data){
+	if(err){
+		callback(err,null);
+	}
+	else{
+		callback(null,data);
+	}
+	});
+}
